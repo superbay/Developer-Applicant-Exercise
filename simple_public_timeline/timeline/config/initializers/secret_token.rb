9da853dbd9fb491489fb32a461aebfdf6d755e01
Ken Lu <ken@seeice.com>
2013-09-15 19:30:21 -0400

    initial rails 4 project of twitter timeline

---
+++ simple_public_timeline/timeline/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Timeline::Application.config.secret_token = 'c1a8db5ba5661e97d694fd5926e18d6f60a3ee88fb6f221f0b8a444009db3694de45635a7ea5ad1dbd82e256e80ac0eaf925c5692135bbdd53b848666cf4e8d0'
