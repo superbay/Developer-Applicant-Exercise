3497d1bdc0c4db4ab2545b5f9897efd6342dba48
Ken Lu <ken@seeice.com>
2013-09-15 19:53:26 -0400

    added some gems

---
+++ simple_public_timeline/timeline/vendor/bundle/gems/em-twitter-0.2.2/lib/em-twitter/version.rb
@@ -0,0 +1,5 @@
+module EventMachine
+  module Twitter
+    VERSION = "0.2.2" unless defined?(EventMachine::Twitter::VERSION)
+  end
+end
---
+++ simple_public_timeline/timeline/vendor/bundle/gems/tweetstream-2.5.0/lib/tweetstream/version.rb
@@ -0,0 +1,3 @@
+module TweetStream
+  VERSION = '2.5.0' unless defined?(TweetStream::VERSION)
+end
