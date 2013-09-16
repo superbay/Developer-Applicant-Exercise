a23794365ae70fbeb080316b7307b745193769de
Ken Lu <ken@seeice.com>
2013-09-15 20:36:19 -0400

    added uid migration

--- simple_public_timeline/timeline/db/schema.rb
+++ simple_public_timeline/timeline/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130916003013) do
+ActiveRecord::Schema.define(version: 20130916003548) do
 
   create_table "tweets", force: true do |t|
     t.string   "name"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20130916003013) do
     t.string   "source"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "uid"
   end
 
 end
