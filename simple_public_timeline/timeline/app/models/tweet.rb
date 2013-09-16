f63f79b1b47b0b2b010c6beae7913663350076dd
Ken Lu <ken@seeice.com>
2013-09-15 21:02:05 -0400

    added another fields

--- simple_public_timeline/timeline/app/models/tweet.rb
+++ simple_public_timeline/timeline/app/models/tweet.rb
@@ -1,2 +1,16 @@
 class Tweet < ActiveRecord::Base
+  def self.fetch_create
+    binding.pry
+    TweetStream::Client.new.sample do |status|
+      tweet = Tweet.find_or_create_by_uid(status.id)
+      tweet.update_attributes(
+        name: status.user.name,
+        screen_name: status.user.screen_name,
+        profile_image_url: status.user.profile_image_url,
+        content: status.text,
+        published_at: status.created_at,
+        source: status.source
+      )
+    end
+  end
 end
--- simple_public_timeline/timeline/db/schema.rb
+++ simple_public_timeline/timeline/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130916003548) do
+ActiveRecord::Schema.define(version: 20130916003819) do
 
   create_table "tweets", force: true do |t|
     t.string   "name"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20130916003548) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "uid"
+    t.string   "content"
   end
 
 end
